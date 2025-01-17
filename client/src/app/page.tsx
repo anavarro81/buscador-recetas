b97c5d295e7ac15a4eed08546fdd776184466986
Dulce Nevarez <dulcenev04@gmail.com>
2025-01-17 09:27:49 -0500

    prueba

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,27 +2,28 @@
 import { EmptyRecipesContainer, RecipeCard, SearchBar } from "@/components";
 import { styled, Typography } from "@mui/material";
 
-const PageContainer = styled('main')({
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: 'center',
-  width: "100%",
-  minHeight: "100vh",
-  padding: '5px'
+const PageContainer = styled("main")({
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    minHeight: "100vh",
+    padding: "5px",
 });
 
 interface receta {
-  title: string;
-  description: string;
-  rate: number;
-  totalRate: number;
-  steps: number;
-  ingredients: number;
+    title: string;
+    description: string;
+    rate: number;
+    totalRate: number;
+    steps: number;
+    ingredients: number;
 }
 
 // const recetas: receta[] = [];
-const recetas: receta[] = [{
+const recetas: receta[] = [
+    {
         title: "Arroz con pollito",
         description:
             "Arroz con pollo hecho con arroz y pollo, tambien se puede hacer con pollo y arroz pero no es lo mismo",
@@ -52,38 +53,38 @@ const recetas: receta[] = [{
 ];
 
 export default function Home() {
-  return (
-    <PageContainer>
-      <Typography
-        variant="h1"
-        sx={{
-          textAlign: "center",
-          margin: "5% 15% 2% 15%",
-        }}
-      >
-        ¡Empieza a crear tu <br/> combinacion perfecta!
-      </Typography>
-      <Typography variant="h3" sx={{ textAlign: "center" }}>
-        Usa las etiquetas o el buscador para encontrar lo que necesitas.
-      </Typography>
+    return (
+        <PageContainer>
+            <Typography
+                variant="h1"
+                sx={{
+                    textAlign: "center",
+                    margin: "5% 15% 2% 15%",
+                }}
+            >
+                ¡Empieza a crear tu <br /> combinacion perfecta!
+            </Typography>
+            <Typography variant="h3" sx={{ textAlign: "center" }}>
+                Usa las etiquetas o el buscador para encontrar lo que necesitas.
+            </Typography>
 
-      <SearchBar sx={{width: '65%', margin: '20px'}} />
+            <SearchBar sx={{ width: "65%", margin: "20px" }} />
 
-      {recetas && recetas.length > 0 ? (
-        recetas.map((receta) => (
-          <RecipeCard
-            key={receta.title}
-            title={receta.title}
-            description={receta.description}
-            rate={receta.rate}
-            totalRate={receta.totalRate}
-            steps={receta.steps}
-            ingredients={receta.ingredients}
-          />
-        ))
-      ) : (
-        <EmptyRecipesContainer />
-      )}
-    </PageContainer>
-  );
+            {recetas && recetas.length > 0 ? (
+                recetas.map((receta) => (
+                    <RecipeCard
+                        key={receta.title}
+                        title={receta.title}
+                        description={receta.description}
+                        rate={receta.rate}
+                        totalRate={receta.totalRate}
+                        steps={receta.steps}
+                        ingredients={receta.ingredients}
+                    />
+                ))
+            ) : (
+                <EmptyRecipesContainer />
+            )}
+        </PageContainer>
+    );
 }
